397c2b957c17815cae39456a4214b4f2dd0b1b68
SergioCaMi <sergiocami84@gmail.com>
2025-04-24 20:48:46 +0200

    ejercicio 1 ok

--- ejercicio-1/app.js
+++ ejercicio-1/app.js
@@ -4,3 +4,25 @@ let dni = "";
 
 /** Leer README */
 
+const fileContent = fs.readFileSync("hacienda.json", "utf-8");
+const contributors = JSON.parse(fileContent);
+// Iteracion 1
+// console.log(contributors);
+contributors.forEach(contributor => {
+    // Iteracion 2
+    // console.log(contributor.nombre);
+    let texto = '';
+    // Iteracion 3
+    if (contributor.dni == process.argv[2]){
+        if (contributor.notificacionesPendientes){
+            texto = `El/La contribuyente ${contributor.nombre} tiene notificaciones pendientes. Se le enviará un correo a ${contributor.email}.\n`;            
+            console.log(texto);
+
+        } else {
+            texto = `El/La contribuyente ${contributor.nombre} NO tiene notificaciones pendientes.\n`;            
+            console.log(texto);
+        }
+    }
+    fs.writeFileSync("notificaciones.txt", texto, { flag: 'a', encoding: 'utf-8' });
+
+});
